e86e62039b2b88fae77e8f20044699f918c380e3
Rishvic Pushpakaran <rishvic@gmail.com>
2025-11-09 23:02:40 +0530

    Simplified scrolling logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { on } from 'svelte/events';
+	import { MediaQuery } from 'svelte/reactivity';
+	import { innerHeight } from 'svelte/reactivity/window';
 	import type { PageProps } from './$types';
 
 	import HandEye from 'phosphor-svelte/lib/HandEye';
@@ -9,22 +11,22 @@
 	let { data }: PageProps = $props();
 
 	let mainEl: HTMLElement | null = null;
-	let headerEl: HTMLElement | null = null;
 
-	let mainHeight = $state(1);
-	let headerHeight = $state(1);
+	const medium = new MediaQuery('min-width: 48rem');
+
 	let mainScrollY = $state(0);
+	let headerHeight = $derived(medium.current ? 96 : 80);
 
-	function computeOffset(mainHeight: number, headerHeight: number, mainScrollY: number) {
-		if (Math.abs(mainHeight) < 0.01) {
-			return 0;
+	function computeOffset(mainHeight: number | undefined, mainScrollY: number) {
+		if (mainHeight === undefined) {
+			return `calc(50vh - ${mainScrollY}px / 2)`;
 		}
-		return Math.max(0, ((1 - mainScrollY / mainHeight) * (mainHeight - headerHeight)) / 2);
+		return `calc((50vh - ${headerHeight}px / 2) * ${1 - mainScrollY / mainHeight})`
 	}
 
-	function computePercent(mainScrollY: number, mainHeight: number) {
-		if (Math.abs(mainHeight) < 0.01) {
-			return '100%';
+	function computePercent(mainHeight: number | undefined, mainScrollY: number) {
+		if (mainHeight === undefined || Math.abs(mainHeight) < 0.01) {
+			return '0%';
 		}
 		return `${Math.min(100, Math.max((mainScrollY / mainHeight) * 100, 0))}%`;
 	}
@@ -37,12 +39,7 @@
 
 	function measureVariables() {
 		if (mainEl) {
-			mainHeight = mainEl.offsetHeight;
 			mainScrollY = mainEl.scrollTop;
-		}
-
-		if (headerEl) {
-			headerHeight = headerEl.offsetHeight;
 		}
 	}
 
@@ -57,33 +54,27 @@
 	onscroll={handleMainScroll}
 	class="relative h-screen snap-y snap-proximity overflow-y-scroll scroll-smooth md:snap-mandatory"
 	style:--header-height={`${headerHeight}px`}
-	style:--main-height={`${mainHeight}px`}
-	style:--header-offset={`${computeOffset(mainHeight, headerHeight, mainScrollY)}px`}
-	style:--main-scroll-y={`${mainScrollY}px`}
-	style:--scroll-percent={computePercent(mainScrollY, mainHeight)}
-	style:--app-bar-bg-light="color-mix(in oklab, var(--color-icy-wisp) var(--scroll-percent),
-	var(--color-cloud-drift))"
-	style:--app-bar-bg-dark="color-mix(in oklab, var(--color-deep-ocean) var(--scroll-percent),
-	var(--color-midnight-slate))"
+	style:--header-offset={computeOffset(innerHeight.current, mainScrollY)}
+	style:--app-bar-bg-light={`rgb(from var(--color-icy-wisp) r g b / ${computePercent(innerHeight.current, mainScrollY)})`}
+	style:--app-bar-bg-dark={`rgb(from var(--color-deep-ocean) r g b / ${computePercent(innerHeight.current, mainScrollY)})`}
 >
 	<header
-		bind:this={headerEl}
-		class="fixed top-0 z-10 w-screen flex-auto translate-y-(--header-offset)"
+		class="fixed top-0 z-10 h-(--header-height) w-screen flex-auto translate-y-(--header-offset)"
 	>
-		<nav
-			class="flex h-20 flex-row items-center justify-center bg-(--app-bar-bg-light) md:h-28 dark:bg-(--app-bar-bg-dark)"
-		>
+		<nav class="flex h-(--header-height) flex-row items-center justify-center">
 			{#if data.isConspiracy}
 				<HandEye
 					color="currentColor"
 					weight="fill"
-					class="h-16 w-16 text-golden-zing md:h-24 md:w-24"
+					size={headerHeight - 16}
+					class="text-golden-zing"
 				/>
 			{:else}
 				<HandPeace
 					color="currentColor"
 					weight="fill"
-					class="h-16 w-16 text-golden-zing md:h-24 md:w-24"
+					size={headerHeight - 16}
+					class="text-golden-zing"
 				/>
 			{/if}
 		</nav>
@@ -91,7 +82,18 @@
 	<section
 		class="h-screen snap-start bg-(--app-bar-bg-light) dark:bg-(--app-bar-bg-dark)"
 	></section>
-	<section class="h-screen snap-start pt-(--header-height)">Bye</section>
+	<section class="h-screen snap-start">
+		<div class="h-20 bg-(--app-bar-bg-light) md:h-24 dark:bg-(--app-bar-bg-dark)"></div>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget dignissim magna, et
+			eleifend elit. Aenean interdum libero eu nulla tempor ullamcorper. Donec id nibh eget lectus
+			lacinia imperdiet. Morbi sodales viverra sapien sed tempor. Nam fermentum, odio non
+			sollicitudin tincidunt, dolor nulla congue justo, eu facilisis nulla magna in metus. Nam ipsum
+			odio, feugiat vitae condimentum in, euismod ut nibh. Integer ultrices turpis arcu, non egestas
+			libero suscipit dignissim. Vestibulum sapien turpis, euismod a porttitor eget, fermentum non
+			sapien.
+		</p>
+	</section>
 </main>
 
 <style>
