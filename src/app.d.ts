676bef24be601160c37890138829ace67f912e19
Rishvic Pushpakaran <rishvic@gmail.com>
2025-11-11 02:18:29 +0530

    Added Cloudflare config

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,7 +11,7 @@ declare global {
 				COUNTER: DurableObjectNamespace;
 			};
 			context: {
-				waitUntil(promise: Promise<any>): void;
+				waitUntil(promise: Promise<unknown>): void;
 			};
 			caches: CacheStorage & { default: Cache };
 		}
