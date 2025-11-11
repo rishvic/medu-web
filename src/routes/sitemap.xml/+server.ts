fc2c1f5826edbb37eb464287fd0644e5c4704766
Rishvic Pushpakaran <rishvic@gmail.com>
2025-11-11 08:09:32 +0530

    Removed logging from sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,7 +4,6 @@ import { Readable } from 'node:stream';
 import { DATE_MODIFIED } from '$lib/server/constants';
 
 export const GET: RequestHandler = async ({ url }) => {
-	console.log(url);
 	const links = [{ url: '/', changefreq: 'daily', priority: 0.8, lastmod: DATE_MODIFIED }];
 
 	const stream = new SitemapStream({ hostname: url.origin });
