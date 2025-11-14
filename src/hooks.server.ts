8fee2bd76ccd6b2583f546635c581f1c6ad8c3c4
Rishvic Pushpakaran <rishvic@gmail.com>
2025-11-14 18:35:01 +0530

    Preload only latin fonts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
 
+const fontPatterns = [
+	/\/nunito-latin-wght-normal\.[A-Za-z0-9_-]+\.woff2$/,
+	/\/pacifico-latin-400-normal\.[A-Za-z0-9_-]+\.woff2$/,
+	/\/pacifico-latin-400-normal\.[A-Za-z0-9_-]+\.woff$/
+];
+
 export const handle: Handle = async ({ event, resolve }) => {
 	return resolve(event, {
-		preload: ({ type }) => type === 'font'
+		preload: ({ type, path }) =>
+			type === 'font' && fontPatterns.some((fontPattern) => fontPattern.test(path))
 	});
 };
