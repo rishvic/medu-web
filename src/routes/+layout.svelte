b7b1b6c4c252ffde6dd051ef544beb548dedda69
Rishvic Pushpakaran <rishvic@gmail.com>
2025-11-23 05:37:31 +0530

    feat: add csp to the website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,15 +8,6 @@
 	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 	<link rel="shortcut icon" href="/favicon.ico" />
-
-	<!-- Privacy-friendly analytics by Plausible -->
-	<script async src="https://plausible.io/js/pa-vnCyH5YgHamgjYmVN_aZy.js"></script>
-	<script>
-		// prettier-ignore
-		window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-		// prettier-ignore
-		plausible.init()
-	</script>
 </svelte:head>
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		csp: {
+			mode: 'nonce',
+			directives: {
+				'script-src': ['strict-dynamic', 'https:'],
+				'object-src': ['none'],
+				'base-uri': ['none']
+			}
+		}
+	}
 };
 
 export default config;
